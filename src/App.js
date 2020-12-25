be00ce7578613ec1e4e72676bfd67b4633ea8f37
porkyfly <tylerli800@gmail.com>
2020-12-24 23:09:51 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome, Bounty Hunter</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="shopping-list">
+          <h1>Active bounties, 1mi radius:</h1>
+          <ul>
+            <li>Take a pic of the light show line</li>
+            <li>Write down "Bob" on the waitlist</li>
+            <li>Find out if Pho75 (cashonly) takes venmo</li>
+          </ul>
+        </div>
       </header>
     </div>
   );
