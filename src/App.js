573eb4bd677f49609a4eaae4829250f193bbbfe9
porkyfly <tylerli800@gmail.com>
2020-12-27 19:03:00 -0500

    unhardcoded list of bounties

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//displays list of bounties as UI element
+function BountyList(props) {
+  const bounties = props.bounties;
+  const bountyItems = bounties.map((bounty) =>
+    <li>{bounty}</li>
+  );
+  return (
+    <ul>{bountyItems}</ul>
+  )
+}
+
+//list of bounties are stored here
+const bounties = [
+  "Take a pic of the light show line",
+  "Write down \"Bob\" on the waitlist",
+  "Find out if CashOnlyEatery takes venmo"
+];
+
 function App() {
   return (
     <div className="App">
@@ -9,11 +28,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div className="shopping-list">
           <h1>Active bounties, 1mi radius:</h1>
-          <ul>
-            <li>Take a pic of the light show line</li>
-            <li>Write down "Bob" on the waitlist</li>
-            <li>Find out if Pho75 (cashonly) takes venmo</li>
-          </ul>
+          <BountyList bounties={bounties} />
         </div>
       </header>
     </div>
