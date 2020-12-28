9bddd0cd10f099c4c46174ae096f2c1062cf5710
porkyfly <tylerli800@gmail.com>
2020-12-28 18:15:17 -0500

    consolidated into app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
-import CreateBounty from './components/CreateBounty/CreateBounty';
+import { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome, Bounty Hunter</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="bounties-list">
-          <CreateBounty />
-        </div>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      items: [
+        {mission: 'fry egg', coords: '39.234,160.442'}
+      ],
+       formTextMission: '', formTextCoords: ''};
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome, Bounty Hunter</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Active bounties, 1mi radius:</h3>
+          <TodoList items={this.state.items} />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              name="formTextMission"
+              onChange={this.handleInputChange}
+              value={this.state.formTextMission}
+            />
+            <input
+              name="formTextCoords"
+              onChange={this.handleInputChange}
+              value={this.state.formTextCoords}
+            />
+            <button>
+              Add Bounty
+            </button>
+          </form>
+        </header>
+      </div>
+    );
+  }
+
+
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    //edge case handling
+    e.preventDefault();
+    if (this.state.formTextMission.length === 0 || this.state.formTextCoords.length === 0) {
+      return;
+    }
+
+    //construct new item here
+    const newItem = {
+      mission: this.state.formTextMission,
+      coords: this.state.formTextCoords
+    };
+
+    //insert new item, clear form text
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      formTextMission: '',
+      formTextCoords: ''
+    }));
+  }
+
+}
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li>{item.mission} {item.coords}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default App;
+
+
---
+++ src/components/Bounty/Bounty.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Bounty.module.css';
+
+class Bounty extends Component {
+  render() {
+    <div className={styles.Bounty}>
+      Placeholder for evidence here
+    </div>
+  }
+}
+export default Bounty;
